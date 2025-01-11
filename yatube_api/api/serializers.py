394deb16d2571348d0fbc94f47c82386430bb4cc
Heim_Sendir <alex.noa29@gamil.com>
2025-01-11 11:19:31 +0500

    add serializer for api

---
+++ yatube_api/api/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+
+from posts.models import Post, Group, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = (
+            'text',
+            'author',
+            'image',
+            'group'
+        )
+        read_only_fileds = ('author',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('title', 'slug', 'description')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = (
+            'author',
+            'post',
+            'text',
+            'created'
+        )
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import mixins
+
+from .models import Cat, Owner
+from .serializers import CatSerializer, OwnerSerializer, CatListSerializer
 
 # Create your views here.
