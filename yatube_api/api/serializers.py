d8a1736ccf4523a9cf791eef9d09792270fcf1f4
Heim_Sendir <alex.noa29@gamil.com>
2025-01-11 13:15:22 +0500

    fix serializers

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -4,6 +4,11 @@ from posts.models import Post, Group, Comment
 
 
 class PostSerializer(serializers.ModelSerializer):
+    group = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Group.objects.all(),
+        required=False
+    )
 
     class Meta:
         model = Post
@@ -20,16 +25,22 @@ class GroupSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Group
-        fields = ('title', 'slug', 'description')
+        fields = ('id', 'title', 'slug', 'description')
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
         model = Comment
         fields = (
+            'id',
             'author',
             'post',
             'text',
             'created'
         )
+        read_only_fields = ('author',)
